e60862339eb79c20f775a3cb96fb3f46689046d1
arun <arunkumarreddyc2@gmail.com>
2022-01-22 21:41:46 +0530

    second commit

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
-let button = document.getElementsByClassName("btn");
-let result = document.getElementsByClassName("result");
+// Different elements and an url are associated to variables 
+let button = document.getElementsByClassName("btn")[0];
+let result = document.getElementsByClassName("result")[0];
 const url = "https://animechan.vercel.app/api/random";
+result.innerHTML ='click the above button to get the anime quote'
 
-// on clicking the button we should get the response through the following logic
-button.addEventListener("click",getdata())
+// on clicking the button we should get the response 
+button.addEventListener("click",getdata)
 
-
+// Below function is using fetching with async and await
 async function getdata() {
 result.innerHTML ='loading...'
     try{
         let response = await fetch(url);
         let object = await response.json();
-        console.log(object);
-        result.innerHTML = `<h1>${object[0]}</h1>`
+        // console.log(object);
+        result.innerHTML = `<div><h3>${object.quote}</h3> </div>
+        <div class=sub-elements>
+        <p>Anime : <span>${object.anime}</span></p>
+        <p>Character : <span>${object.character}</span></p>
+        <div>`
 
     }catch(error){
         console.log(error)
     }
 }
-
